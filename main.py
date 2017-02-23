7f844343440e55fe3113186568e51c748e573e78
Liam O'Reilly <L.P.OReilly@swansea.ac.uk>
2017-02-23 19:23:03 +0000

    parse basic data

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ FILENAME = "examples/me_at_the_zoo.in"
 infile = open(FILENAME)
 
 generalSpecLine = infile.readline()
+generalSpecLineParts = generalSpecLine.split()
+
+numVideos = generalSpecLineParts[0]
+numEndPoints = generalSpecLineParts[1]
+numRequestDescriptions = generalSpecLineParts[2]
+numCaches = generalSpecLineParts[3]
+
 
 infile.close()
 
-print(generalSpecLine)
+print("Number of Videos:", numVideos)
+print("Number of End Points:", numEndPoints)
+print("Number of Request Descriptions:", numRequestDescriptions)
+print("Number of caches:", numCaches)
