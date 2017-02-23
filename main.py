b9e8378724d26c5e389ba6b972d81706b206c5e4
Liam O'Reilly <L.P.OReilly@swansea.ac.uk>
2017-02-23 19:49:50 +0000

    trying to read file caches

--- main.py
+++ main.py
@@ -1,21 +1,54 @@
 FILENAME = "examples/me_at_the_zoo.in"
 
+class EndPoint:
+    def __init__(self, endPointDataCenterLink, numCaches) :
+        self._endPointDataCenterLink = endPointDataCenterLink
+        self._numCaches = numCaches
+        self._cacheLinks = {}
+        
+    def __str__(self):
+        s = "End Point [data center = " + str(self._endPointDataCenterLink) + ", numCaches = " + str(self._numCaches) + "]"
+        for cache in self._cacheLinks.keys() :
+            s = "\t\t" + str(cache) + " : " +  str(self._cacheLinks.get(cache))
+        return s
+
+    def addCacheLink(self, cacheNum, delay):
+        self._cacheLinks[cacheNum] = delay
+
 infile = open(FILENAME)
 
 # Parse basic data
 generalSpecLine = infile.readline()
 generalSpecLineParts = generalSpecLine.split()
 
-numVideos = generalSpecLineParts[0]
-numEndPoints = generalSpecLineParts[1]
-numRequestDescriptions = generalSpecLineParts[2]
-numCaches = generalSpecLineParts[3]
+numVideos = int(generalSpecLineParts[0])
+numEndPoints = int(generalSpecLineParts[1])
+numRequestDescriptions = int(generalSpecLineParts[2])
+numCaches = int(generalSpecLineParts[3])
 
 # parse video sizes
 videoSizesLine = infile.readline()
 videosSizes = map(int, videoSizesLine.split())
 
+# Read end point data
 
+endPoints = []
+
+for endPointIndex in range(numEndPoints):
+    endPointSpecLine = infile.readline()
+    endPointSpecLineParts =endPointSpecLine.split()
+    endPointDataCenterLink = int(endPointSpecLineParts[0])
+    endPointNumCache = int(endPointSpecLineParts[1])
+    
+    endPoint = EndPoint(endPointDataCenterLink, endPointNumCache)
+    endPoints.append(endPoint)
+    
+    for epCacheNum in range(endPointNumCache) :
+        epCacheSpec = infile.readline()
+        epCacheSpecParts = epCacheSpec.split()
+        x = int(epCacheSpecParts[0])
+        delay = int(epCacheSpecParts[1])
+        endPoint.addCacheLink(x, delay)
 
 infile.close()
 
@@ -25,3 +58,8 @@ print("Number of Request Descriptions:", numRequestDescriptions)
 print("Number of caches:", numCaches)
 
 print("Video Sizes:", videosSizes)
+
+print("End Points:")
+for i in range(len(endPoints)) :
+    print("End Point " + str(i) + ":")
+    print("\t", str(endPoints[i]))
